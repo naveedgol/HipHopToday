1c698294f677b71baf7dc2e3b72a033f2d5e20b1
Naveed Golafshani <ngolafshani@live.ca>
2017-01-13 14:57:50 -0500

    Pushes song info to html file

--- main.py
+++ main.py
@@ -28,7 +28,27 @@ def date_formatter(date):
     formatted_date = year + "-" + month + "-" + day
     return formatted_date
 
+def write_to_html(info):
+    html_file = open('index.html', "r+")
+    html_doc_soup = BeautifulSoup(html_file, "html.parser")
+    counter = 0
+    for i in html_doc_soup.findAll("p"):
+        i.string += info[counter]
+        counter += 1
+    html_file.close()
+    html_output = html_doc_soup.prettify("utf-8")
+    with open("index.html", "wb") as file:
+        file.write(html_output)
+
+day_counter = 0
+# date = [""]*11
+song_info = [""]*11
 for i in range(0,11):
-    closest_saturday = today_date_adjuster(datetime.datetime.today() - datetime.timedelta(days=365*i))
-    print(date_formatter(datetime.datetime.today() - datetime.timedelta(days=365*i)))
-    print(scrape_number_1("http://www.billboard.com/charts/r-b-hip-hop-songs/" + closest_saturday))
+    if i == 1 or i == 5 or i == 9:
+        day_counter += 1
+    closest_saturday = today_date_adjuster(datetime.datetime.today() - datetime.timedelta(days=day_counter))
+    # date[i] = (datetime.datetime.today() - datetime.timedelta(days=day_counter)).year
+    song_info[i] = scrape_number_1("http://www.billboard.com/charts/r-b-hip-hop-songs/" + closest_saturday)
+    day_counter += 365
+
+write_to_html(song_info)
